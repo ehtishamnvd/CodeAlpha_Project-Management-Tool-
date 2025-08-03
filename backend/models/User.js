58a0bcfae1b0c4afd55eda49152c6f7116fcd925
Ehtisham Naveed <nehtisham7@gmail.com>
2025-08-03 05:34:20 +0500

    Initial commit

---
+++ backend/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Email is required']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required']
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
+}, { timestamps: true });
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
